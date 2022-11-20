904e56b9404c616541a52ebd1c2ee722c02582ab
CadeJordan <cademalcolmjordan@googlemail.com>
2022-11-20 16:47:15 +0000

    add JavaScript

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);}
 
-if (topic === 'HTML') {
+/*
+    if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
   console.log("Let's study CSS!");
@@ -12,3 +13,4 @@ if (topic === 'HTML') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');}
+  */
